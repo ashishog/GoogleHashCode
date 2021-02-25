3484e478d378aa87a71a20c1fca4c76b171afc6d
Piyush01-ui <79646444+Piyush01-ui@users.noreply.github.com>
2021-02-25 21:24:38 +0530

    Pizza

--- 0=hello.cpp
+++ 0=hello.cpp
@@ -1,10 +1,18 @@
-#include <iostream>
-
+#include<iostream>
+#include<string.h>
+#include<stdio.h>
 using namespace std;
-
+int main()
 int main()
 {
-
-cout<<"asrgasgvgebyv df";
-    return 0;
+  int i;
+  char Pizza0[3][10],Pizza1[3][10];
+  cin>>std1;
+  for(i=0;i<3;i++)
+    gets(Pizza0[i]);
+    cout<<"\n\n";
+  for(j=0;j<3;j++)
+    gets(Pizza1[j]);
+    cout<<"\n\n";
+  funcomp(Pizza0,Pizza1)  
 }
