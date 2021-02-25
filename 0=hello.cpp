dd9e6ebf75ff4985234a2d86e3b9da4f2b6257b4
Piyush01-ui <79646444+Piyush01-ui@users.noreply.github.com>
2021-02-25 21:55:36 +0530

    Update 0=hello.cpp

--- 0=hello.cpp
+++ 0=hello.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 int main()
 {
-  int i;
+  int i,j;
   char Pizza0[3][10],Pizza1[3][10];
   cin>>std1;
   for(i=0;i<3;i++)
@@ -14,7 +14,6 @@ int main()
   for(j=0;j<3;j++)
     gets(Pizza1[j]);
     cout<<"\n\n";
-  funcomp(Pizza0,Pizza1)  
     cout<<"Hello ANPIKUAS";
 }
 
