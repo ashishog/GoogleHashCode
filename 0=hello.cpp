52bddfe10c7a8b6f4c58de88d3427b5f125825eb
Piyush01-ui <79646444+Piyush01-ui@users.noreply.github.com>
2021-02-25 22:16:25 +0530

    Update 0=hello.cpp

--- 0=hello.cpp
+++ 0=hello.cpp
@@ -5,15 +5,19 @@ using namespace std;
 int main()
 int main()
 {
-  int i,j;
+  int i,j,p,cnt=0;
   char Pizza0[3][10],Pizza1[3][10];
+
   for(i=0;i<3;i++)
     gets(Pizza0[i]);
     cout<<"\n\n";
   for(j=0;j<3;j++)
     gets(Pizza1[j]);
     cout<<"\n\n";
-    cout<<"Hello ANPIKUAS";
+  //cout<<funcomp(&Pizza0[3][10],&Pizza1[3][10]);
+  if(strcmp(Pizza0[1],Pizza1[1])==0)
+        cnt++;
+  cout<<cnt;
 }
 
 #include<iostream> 
