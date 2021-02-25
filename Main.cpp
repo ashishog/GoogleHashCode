fab6bcc957970c1070db8b6955a18e7c6483e3ae
Piyush01-ui <79646444+Piyush01-ui@users.noreply.github.com>
2021-02-26 00:27:26 +0530

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,23 @@
-#include<iostream.h>
+#include<iostream>
 #include<stdio.h>
-void main()
+using namespace std;
+struct street
 {
-  int D,I,S,V,F,i;
+ int B,E;
+ char str1[10000];
+};
+int main()
+{
+    long long int D,I,S,V,F;
+    cin>>D>>I>>S>>V>>F;
+    struct street st[S];
+    for(int i=0;i<S;i++)
+    {
+        cin>>st[i].B>>st[i].E;
+        gets(st[i].str1);
+    }
+        return 0;
+}
+
   
   
