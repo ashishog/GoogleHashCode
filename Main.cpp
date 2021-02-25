858fc8c445e407ddca14830618e9677f2d900eb5
Piyush01-ui <79646444+Piyush01-ui@users.noreply.github.com>
2021-02-26 00:34:43 +0530

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -3,18 +3,23 @@
 using namespace std;
 struct street
 {
- int B,E;
- char str1[10000]; };
-int main(){
+ int B,E,L;
+ char str1[10000];
+};
+int main()
+{
     long long int D,I,S,V,F;
     cin>>D>>I>>S>>V>>F;
     struct street st[S];
     for(int i=0;i<S;i++)
     {
+        cout<<"\n";
         cin>>st[i].B>>st[i].E;
         gets(st[i].str1);
+        cin>>st[i].L;
+        cout<<"\n";
     }
-    return 0;
+        return 0;
 }
 
   
